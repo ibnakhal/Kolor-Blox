1ae81bc414eef1af02be886b50d14b95cbbc0b70
Ibrahim Nakhal <INAKHAL2@art.edu>
2018-09-11 16:17:21 -0700

    sprites modified. the need to be more bubbly

--- Assets/Handler.cs
+++ Assets/Handler.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Handler : MonoBehaviour {
+public class Handler : MonoBehaviour
+{
     public List<ColorChanger> blocks;
     public int counter;
     public int total;
@@ -11,28 +12,28 @@ public class Handler : MonoBehaviour {
     public Text counterText;
     public List<int> scores;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(counter == 0)
-        {
-            Debug.Log("lose");
-        }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
         for (int i = 0; i < blocks.Count; i++)
         {
             for (int x = 0; x < blocks.Count; x++)
             {
-                if(blocks[i].Kolor != blocks[x].Kolor)
+                if (blocks[i].Kolor != blocks[x].Kolor)
                 {
                     return;
                 }
             }
         }
 
-        Debug.Log("win");
+
+        End();
 
     }
 
@@ -42,4 +43,30 @@ public class Handler : MonoBehaviour {
         Debug.Log(counter);
         counterText.text = counter.ToString();
     }
+
+    public void End()
+    {
+        if (counter < scores[0])
+        {
+            //3star
+            Debug.Log("THREE STARS!");
+        }
+        if (counter >= scores[0] && counter < scores[1])
+        {
+            //2star
+            Debug.Log("TWO STARS!");
+
+        }
+        if (counter >= scores[1] && counter < scores[2])
+        {
+            //1star
+            Debug.Log("ONE STAR!");
+        }
+        if (counter <= scores[2])
+        {
+            //0star
+            Debug.Log("NO STARS!");
+        }
+    }
+
 }
